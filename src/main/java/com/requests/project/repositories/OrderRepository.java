63adbf142dcd8423afd1ea6d02edca8169837b71
Lucas Diniz <lucasfonseca108.lf@gmail.com>
2023-11-19 01:28:36 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/requests/project/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.requests.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.requests.project.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
---
+++ src/main/java/com/requests/project/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.requests.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.requests.project.entities.Order;
+import com.requests.project.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
