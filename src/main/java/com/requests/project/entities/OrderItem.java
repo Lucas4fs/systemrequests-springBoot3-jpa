d3d907360e33957d952b1f6329634b41d5a0a2d8
Lucas Diniz <lucasfonseca108.lf@gmail.com>
2023-11-23 07:29:24 -0300

    Subtotal & Total methods atualizado

--- src/main/java/com/requests/project/entities/OrderItem.java
+++ src/main/java/com/requests/project/entities/OrderItem.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_order_item")
+@Table(name = "tb_order_item")//
 public class OrderItem implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@EmbeddedId
