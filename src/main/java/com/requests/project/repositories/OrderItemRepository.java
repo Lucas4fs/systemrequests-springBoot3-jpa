35bd38f073b0b3040cee1c77ac2d028ec120cfe2
Lucas Fonseca <96666996+Lucas4fs@users.noreply.github.com>
2024-01-19 15:41:00 -0300

    Update OrderItemRepository.java

--- src/main/java/com/requests/project/repositories/OrderItemRepository.java
+++ src/main/java/com/requests/project/repositories/OrderItemRepository.java
@@ -10,12 +10,12 @@ import com.requests.project.entities.OrderItem;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 	@Query(value = "SELECT id AS id,"
-				 + "(price * quantity) AS orderItemTotal,"
-				 + "price AS productPrice,"
-				 + " quantity AS orderItemQuantity,"
-				 + " order_id AS orderId,"
-				 + " product_id AS productId"
-				 + " FROM tb_order_item"
-				 , nativeQuery = true)
+		     + "(price * quantity) AS orderItemTotal,"
+		     + "price AS productPrice,"
+		     + " quantity AS orderItemQuantity,"
+		     + " order_id AS orderId,"
+		     + " product_id AS productId"
+		     + " FROM tb_order_item"
+		     , nativeQuery = true)
 	List<InterfaceOrderItem> searchAll();
 }
